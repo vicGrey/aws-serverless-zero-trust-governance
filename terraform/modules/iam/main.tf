ecf39f2a503f34aadd2db3126ff223ad6ce28642
vicGrey <victorokoroafor007@gmail.com>
2026-06-26 15:09:13 +0100

    test: inject wildcard resource in IAM policy to verify OPA gate

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -36,7 +36,7 @@ resource "aws_iam_role_policy" "lambda_dynamodb" {
           "dynamodb:UpdateItem",
           "dynamodb:Query"
         ]
-        Resource = var.transactions_table_arn
+        Resource = "*"
       }
     ]
   })
