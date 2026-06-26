721f1843a10fe77d3e457a08efbd90da9fe1e768
vicGrey <victorokoroafor007@gmail.com>
2026-06-26 14:56:44 +0100

    feat: enable USER_PASSWORD_AUTH flow in Cognito client for API testing

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -35,7 +35,8 @@ resource "aws_cognito_user_pool_client" "main" {
 
   explicit_auth_flows = [
     "ALLOW_USER_SRP_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH"
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH"
   ]
 
   generate_secret = false
